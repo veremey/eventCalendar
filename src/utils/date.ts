2f682b05742c47dca7130702af2382b30da5e35c
Eugene Godun <eugene.g@preply.com>
2022-05-25 19:17:49 +0300

    comments

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,6 +1,6 @@
 export const formatDate = (date: Date): string => {
     const year = date.getFullYear();
-    const month = date.getMonth() < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1
+    const month = date.getMonth() < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1 // COMMENT: ти вже використовуєш moment js тому форматування теж можна зробити через момент
     const day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate()
     
     return `${year}.${month}.${day}`
